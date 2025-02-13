93ff1fe2e2ccadf52d44d5fe4b2b04328acda046
Sam Chan <chansa6@oocl.com>
2025-02-13 15:46:22 +0800

    fix: string interpolation issue

--- vars/buildUtils.groovy
+++ vars/buildUtils.groovy
@@ -1,13 +1,13 @@
 def buildMaven(String pomLocation = "$WORKSPACE/app") {
-    echo 'Maven Build with pomLocation: ${pomLocation}'
+    echo "Maven Build with pomLocation: ${pomLocation}"
     configFileProvider([configFile(fileId: 'bbbe1198-7511-421a-b98d-13af7cc08480', variable: 'MAVEN_SETTINGS')]) {
-        sh '''
+        sh """
         cd ${pomLocation}
         mvn -s $MAVEN_SETTINGS clean install \
             -Dmaven.wagon.http.ssl.insecure=true \
             -X
         mvn -s $MAVEN_SETTINGS jacoco:prepare-agent test jacoco:report -X
-        '''
+        """
     }
 }
 
